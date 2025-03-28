bcd16da2881d3b104e125716415a1dab1230609b
Pontus Österdahl <pos@win.su.se>
2025-03-28 11:12:42 +0100

    add contact

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 $(function(){
-    $("<h1>GNOMIKA</h1><div class=\"titleinformation\"><nav><ul><li><a href=\"introduction.html\">Introduction</a></li> <li><a href=\"mss_list.html\">List of manuscripts</a></li> <li><a href=\"collections.html\">Transcriptions</a></li> <li><a href=\"integated_editions.html\">Edition</a></li><li>Concordance</li><li>Bibliography</li></ul></nav></div>").
+    $("<h1>GNOMIKA</h1><div class=\"titleinformation\"><nav><ul><li><a href=\"introduction.html\">Introduction</a></li> <li><a href=\"mss_list.html\">Manuscripts</a></li> <li><a href=\"collections.html\">Transcriptions</a></li> <li><a href=\"integated_editions.html\">Edition</a></li><li>Concordance</li><li>Bibliography</li><li>Contact</li></ul></nav></div>").
     prependTo(".dummyHeader");
   });
 
